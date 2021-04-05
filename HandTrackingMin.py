dee3388f07d7e9fcb725f48b2435e05a52367994
Captluke <lukasjohnny2328@gmail.com>
2021-04-05 16:37:21 +0800

    +Added Multi Hand Landmarks Detection landmarks

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,13 +5,16 @@ import time
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, image = cap.read()
     imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     print(results.multi_hand_landmarks)
-
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_axis(image, handLms, mpHands.HAND_CONNECTIONS)
     cv2.imshow("Results", image)
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
