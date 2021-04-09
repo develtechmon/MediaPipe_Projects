068c5e4555450d0865d3bdfc1964bd0e6c53ea8b
Captluke <lukasjohnny2328@gmail.com>
2021-04-10 00:57:29 +0800

    + add tab

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -32,7 +32,6 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-
     cv2.putText(image, str(int(fps)),(10,60), cv2.FONT_HERSHEY_PLAIN,3, (255,0,255),4)
 
     cv2.imshow("Results", image)
